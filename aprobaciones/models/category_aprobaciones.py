44b7688e592e33010047c6dbbcf210a9fce30fa2
emilmar@otc.meme <emilmarpatricia@gmail.com>
2026-03-12 10:45:14 -0400

    add: inheritance with contact

--- aprobaciones/models/category_aprobaciones.py
+++ aprobaciones/models/category_aprobaciones.py
@@ -5,3 +5,5 @@ from odoo import fields, models
 class Category(models.Model):
     _name='aprobaciones.category'
     name=fields.Char(string="Nombre de la categoria")
+    is_category_aprobacion=fields.Boolean(string="Categoria de aprobacion")
+    is_important= fields.Boolean(string="Es importante")
